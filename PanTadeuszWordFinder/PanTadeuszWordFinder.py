a17b472845e252e1cd9f2b858e58fa8909c26d3b
Darek <dariusz.duda.dd@gmail.com>
2017-12-12 18:22:22 +0100

    Print number of lines

--- PanTadeuszWordFinder/PanTadeuszWordFinder.py
+++ PanTadeuszWordFinder/PanTadeuszWordFinder.py
@@ -1,3 +1,11 @@
-my_words = "Pan"
+my_words = [elt.strip() for elt in open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt","r").readlines()]
+word_list = [elt.strip() for elt in open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\words-list.txt","r").readlines()]
 
-Print(my_words)
+counter = 0
+
+for word in my_words:
+    if word not in word_list:
+        print(word)
+        counter += 1
+
+print("Number of lines : %d" % counter)
