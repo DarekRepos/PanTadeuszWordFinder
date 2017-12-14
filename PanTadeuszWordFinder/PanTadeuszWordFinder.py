91b4d55e638ea469d7ead00d1ec26e020abfba82
Darek <dariusz.duda.dd@gmail.com>
2017-12-14 16:49:42 +0100

    Lines without empty  lines

--- PanTadeuszWordFinder/PanTadeuszWordFinder.py
+++ PanTadeuszWordFinder/PanTadeuszWordFinder.py
@@ -1,11 +1,17 @@
-my_words = [elt.strip() for elt in open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt","r").readlines()]
+def nonblank_lines(my_words):
+    for lines in my_words:    
+        line = lines.rstrip()
+        if line:
+            yield line
+
 word_list = [elt.strip() for elt in open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\words-list.txt","r").readlines()]
 
 counter = 0
 
-for word in my_words:
-    if word not in word_list:
-        print(word)
-        counter += 1
+with open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt","r") as epics_words:
+    for word in nonblank_lines(epics_words):
+        if word not in word_list:
+            print(word)
+            counter += 1
 
 print("Number of lines : %d" % counter)
