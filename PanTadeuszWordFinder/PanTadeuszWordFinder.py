518e58059c758ed3de1c6f4ad05f5e67d58cc0e4
DarekRepos <duda.dariusz.dd@gmail.com>
2017-12-18 12:34:24 +0100

    Create collection of object

--- PanTadeuszWordFinder/PanTadeuszWordFinder.py
+++ PanTadeuszWordFinder/PanTadeuszWordFinder.py
@@ -5,13 +5,14 @@ def nonblank_lines(my_words):
             yield line
 
 word_list = [elt.strip() for elt in open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\words-list.txt","r").readlines()]
+word_set = set(word_list)
 
 counter = 0
 
 with open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt","r") as epics_words:
     for word in nonblank_lines(epics_words):
-        if word not in word_list:
-            print(word)
+        if word not in word_set:
+            print (word)
             counter += 1
 
 print("Number of lines : %d" % counter)
