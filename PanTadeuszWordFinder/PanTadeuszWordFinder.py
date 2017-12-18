c54666aff3ba958c64b97e308d2431d484641c46
DarekRepos <duda.dariusz.dd@gmail.com>
2017-12-18 13:03:08 +0100

    Add relative path to project files

--- PanTadeuszWordFinder/PanTadeuszWordFinder.py
+++ PanTadeuszWordFinder/PanTadeuszWordFinder.py
@@ -1,15 +1,20 @@
+import os
+
 def nonblank_lines(my_words):
     for lines in my_words:    
         line = lines.rstrip()
         if line:
             yield line
 
-word_list = [elt.strip() for elt in open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\words-list.txt","r").readlines()]
+dir_path = os.path.dirname(__file__)
+
+
+word_list = [elt.strip() for elt in open(dir_path + "\words-list.txt","r").readlines()]
 word_set = set(word_list)
 
 counter = 0
 
-with open("E:\Programy Python\PanTadeuszWordFinder\PanTadeuszWordFinder\pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt","r") as epics_words:
+with open(dir_path + "\pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt","r") as epics_words:
     for word in nonblank_lines(epics_words):
         if word not in word_set:
             print (word)
